f6d9930a70b27b6736827337abf484afa7700ada
AlexandraRamirezz <117787019+AlexandraRamirezz@users.noreply.github.com>
2023-09-05 00:59:21 -0500

    lab06 subido

---
+++ Semana-3.1--MultiList-AlbumDeHeroes-.NET 6.0/Semana-3.1--MultiList-AlbumDeHeroes-.NET 6.0/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_3._1__MultiList_AlbumDeHeroes_.NET_6._0
+{
+    internal class Album
+    {
+        public Album() { }
+
+        public String Codigo { get; set; }
+
+        public String Nombre { get; set; }
+
+        public List<Heroe> Heroes { get; set; }
+    }
+}
